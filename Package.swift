ec088f08b1cc4871ed6380f9ec41b4fc96dbd9af
jermware <jermware@mac.com>
2025-01-08 16:15:46 +0000

    referebce local ignite-0.2.2 package until remote version changes incorporated

--- Package.swift
+++ Package.swift
@@ -7,7 +7,8 @@ let package = Package(
     name: "JermwareWebsite",
     platforms: [.macOS(.v13)],
     dependencies: [
-        .package(url: "https://github.com/twostraws/Ignite.git", branch: "main")
+//        .package(url: "https://github.com/twostraws/Ignite.git", branch: "main")
+        .package(name: "Ignite", path: "/Users/Gavin/Downloads/Ignite-0.2.2")
     ],
     targets: [
         .executableTarget(
