51189b93e7d18666ca494eabf2c580ca47740a3a
jermware <jermware@mac.com>
2025-01-09 10:15:14 +0000

    tag results displayed in a table in reverse date order

--- Sources/Pages/Tags.swift
+++ Sources/Pages/Tags.swift
@@ -24,15 +24,30 @@ struct Tags: TagPage {
             }
             .margin(.top, .large)
 
-            let articles = context.content(tagged: tag)
-            
-            List {
-                for article in articles {
-                    Link(article)
-                        .linkStyle(.hover)
+            Table {
+                for content in context.content(tagged: tag).sorted(by: \.date, order: .reverse) {
+                    Row {
+                        Column {
+                            "\(content.date.asShortDisplay)"
+                        }
+                        .verticalAlignment(.middle)
+                        .style("width: 200px")
+                        Column {
+                            Link("\(content.title)", target: content.path)
+                                .linkStyle(.hover)
+
+                            if let subtitle = content.subtitle {
+                                Text(subtitle)
+                                    .margin(.bottom, 0)
+                            }
+                        }
+                        .verticalAlignment(.middle)
+                    }
                 }
             }
+            .tableStyle(.stripedRows)
             .margin(.top, .large)
+
         }
         .frame(width: "90%")
     }
