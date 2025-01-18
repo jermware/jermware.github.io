9efeaa909abde9efbf2b9633a2f5113b3e41ab3f
jermware <jermware@mac.com>
2025-01-18 09:34:35 +0000

    use local clone, not fork, of ignite package

--- Package.swift
+++ Package.swift
@@ -7,7 +7,9 @@ let package = Package(
     name: "JermwareWebsite",
     platforms: [.macOS(.v13)],
     dependencies: [
-        .package(url: "https://github.com/twostraws/Ignite.git", branch: "main")
+//        .package(url: "https://github.com/twostraws/Ignite.git", branch: "main")
+//        .package(path: "../Ignite-fork")
+        .package(path: "../Ignite")
     ],
     targets: [
         .executableTarget(
